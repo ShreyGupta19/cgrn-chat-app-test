441bfa910d13f532b910b8aa09e9aa0b08b917e6
sgupta7 <sgupta@tgen.org>
2015-02-25 11:00:00 -0700

    Initial Commit. Basic chat application that uses Socket.io, Express, and Node.js. Provides real-time chat communication with 3 different exclusive chat rooms.

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, 'templates', 'index.html'));
+});
+app.post('/',function(req,res){
+	console.log(req.body);
+	res.send({redirect:'/'+req.body.group});
+});
+app.get('/alpha', function(req, res){
+	console.log("hi");
+	res.sendFile(path.join(__dirname, 'templates', 'alpha.html'));
+});
+app.get('/beta', function(req, res){
+	res.sendFile(path.join(__dirname, 'templates', 'beta.html'));
+});
+app.get('/gamma', function(req, res){
+	res.sendFile(path.join(__dirname, 'templates', 'gamma.html'));
+});
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket){
+	socket.on('groupConnect', function(group){
+		var groupNsp = io.of('/' + group);
+	});
+	socket.on('message', function(data){
+		console.log('data: ' + JSON.stringify(data));
+		io.of("/"+data.nsp).emit('message', data.msg);
+	});
+});
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});

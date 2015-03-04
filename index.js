31cb925d9d41c8964cdf1b0ed53010cd7da3fe2e
sgupta7 <sgupta@tgen.org>
2015-03-04 11:39:50 -0700

    Added MongoDB database support along with user authentication and Bootstrap UI Refresh for the Login page. Currently allows for a predetermined group of users to login to one of three pre-provisioned chat rooms. All chat room delivery logic is generalized, so more chat rooms can be generated on the fly later. New user, new chat room, and chat history functionality to be added.

--- index.js
+++ index.js
@@ -4,43 +4,106 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
 var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+var fs = require('fs');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
-console.log(process.env);
+var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/chatdbtest';
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.get('/', function(req, res){
-	console.log("Loading index page...");
-	res.sendFile(path.join(__dirname, 'templates', 'index.html'));
-});
-app.post('/',function(req,res){
-	console.log(req.body);
-	res.send({redirect:'/'+req.body.group});
-});
-app.get('/alpha', function(req, res){
-	console.log("hi");
-	res.sendFile(path.join(__dirname, 'templates', 'alpha.html'));
-});
-app.get('/beta', function(req, res){
-	res.sendFile(path.join(__dirname, 'templates', 'beta.html'));
-});
-app.get('/gamma', function(req, res){
-	res.sendFile(path.join(__dirname, 'templates', 'gamma.html'));
-});
-
 app.use(express.static(__dirname + '/public'));
+app.use(cookieParser());
+app.use(session({secret: '1234567890QWERTY', resave:false, saveUninitialized: false}));
 
-io.on('connection', function(socket){
-	socket.on('groupConnect', function(group){
-		var groupNsp = io.of('/' + group);
+function isEmpty(obj) {
+	var hasOwnProperty = Object.prototype.hasOwnProperty;
+	if (obj == null) return true;
+	if (obj.length > 0) return false;
+	if (obj.length === 0) return true;
+	for (var key in obj) if (hasOwnProperty.call(obj, key)) return false;
+	return true;
+}
+
+mongodb.connect(mongoURI,function(err,db){
+	if(err) throw err;
+	else console.log("Successfully connected to MongoDB.");
+	var users = db.collection('userData');
+	var rooms = db.collection('chatRoomData');
+
+	app.get('/', function(req, res){
+		if(!isEmpty(req.query)){
+			if(req.query.type==="getGroupNames"){
+				rooms.find({},{name:1,_id:0}).toArray(function(err,docs){
+					if(err) throw err;
+					else if(!docs.length){
+						res.send({error:"No chat rooms found."});
+					}
+					else {
+						var groupNames = [];
+						docs.forEach(function(doc){
+							groupNames.push(doc.name);
+						});
+						res.send({names:groupNames});
+					}
+				});
+			}
+		}
+		else res.sendFile(path.join(__dirname, 'templates', 'index.html'));
 	});
-	socket.on('message', function(data){
-		console.log('data: ' + JSON.stringify(data));
-		io.of("/"+data.nsp).emit('message', data.msg);
+	app.post('/',function(req,res){
+		console.log(req.body);
+		users.find({email:req.body["login-email"],password:req.body["login-password"]}).toArray(function(err,docs){
+			if(err) throw err;
+			else if(!docs.length){
+				res.send({error:"Incorrect email or password."});
+			}
+			else if(docs.length > 1){
+				res.send({error:"Your password and email is the same as another members!"});
+			}
+			else {
+				console.log(docs);
+				req.session.user = docs[0];
+				users.update({email:docs[0].email}, {$push:{logins: {"room":req.body["login-group"],"time":new Date().toISOString()}}}, function(err, result){
+					if(err) throw err;
+					res.send({redirect:'/rooms/'+req.body["login-group"]});
+				});
+			}
+		});
 	});
+	var modifyChatFile = function(groupName,routeData){
+		fs.readFile(path.join(__dirname, 'templates', 'chat.html'), {"encoding":'utf8'}, function(err, data){
+			if(err) throw err;
+			data = data.replace(/\"%%%USERDATA%%%\"/g, JSON.stringify(routeData.req.session.user));
+			data = data.replace(/%%%GROUPNAME%%%/g,groupName);
+			data = data.replace(/%%%GROUPNAME%%%/g,groupName);
+			data = data.replace(/%%%GROUPNAME_UPPERCASE%%%/g,groupName.charAt(0).toUpperCase() + groupName.slice(1));
+			routeData.res.send(data);
+		});
+	}
+	app.get('/rooms/:groupName', function(req, res){
+		modifyChatFile(req.params.groupName,{"req":req,"res":res});
+	});
+
+	io.on('connection', function(socket){
+		socket.on('groupConnect', function(data){
+			var groupNsp = io.of('/' + data.group);
+			rooms.update({name:data.group}, {$push:{logins: {"user":data.user,"timeIn":new Date().toISOString()}}}, function(err, result){
+				if(err) throw err;
+			});
+		});
+		socket.on('message', function(data){
+			console.log('data: ' + JSON.stringify(data));
+			var currentTime = new Date().toISOString();
+			rooms.update({name:data.nsp}, {$push:{messages: {"user":data.user.email,"time":currentTime,"content":data.msg}}}, function(err, result){
+				if(err) throw err;
+				io.of("/"+data.nsp).emit('message', {"msg":data.msg, "user":data.user.name, "time":currentTime});
+			});
+		});
+	});
+
 });
 
 http.listen(app.get('port'), function(){
--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
 	console.log(getGroup());
+	console.log(getUserData());
 	var socketGlobal = io.connect(window.location.origin + "/");
-	socketGlobal.emit('groupConnect', getGroup());
-	console.log(window.location);
+	socketGlobal.emit('groupConnect', {"group":getGroup(),"user":getUserData().email});
 	setTimeout(function(){
 		socket = io.connect(window.location.origin + "/" + getGroup());
 		nsp = getGroup();
 		$('form').submit(function(){
-			socketGlobal.emit('message', {"msg":$('#m').val(),"nsp":nsp});
+			socketGlobal.emit('message', {"msg":$('#m').val(),"nsp":nsp, "user":getUserData()});
 			$('#m').val('');
 			return false;
 		});
-		socket.on('message', function(msg){
-			console.log("hey");
-			$('#messages').append($('<li>').text(msg));
+		socket.on('message', function(data){
+			$('#messages').append($('<li>').text(data.user.first + " said " + data.msg));
 		});
 	},1000);
 });
